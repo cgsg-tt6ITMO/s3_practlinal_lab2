c30d661d0e73bf5dde229b8ab56f34d8a2c3d226
ttoomat <troitskayatamara@mail.ru>
2023-10-12 16:13:03 +0300

    Pdf progress

--- src/latex/latexgen.py
+++ src/latex/latexgen.py
@@ -1,5 +1,6 @@
 def latex_matrix_2x2(l):
     if (l == [[],[]]): return
+
     print("$\\begin{pmatrix}")
     print("%.2f & %.2f \\\\" % (l[0][0], l[0][1]))
     print("%.2f & %.2f \\\\" % (l[1][0], l[1][1]))
@@ -8,6 +9,7 @@ def latex_matrix_2x2(l):
 
 def latex_matr_2x2(l):
     if (l == [[],[]]): return
+
     print("$\\begin{pmatrix}")
     print("%d & %d \\\\" % (l[0][0], l[0][1]))
     print("%d & %d \\\\" % (l[1][0], l[1][1]))
@@ -22,7 +24,7 @@ def latex_vector(v):
 
 
 #inputs matrix from file
-f = open('Z:\practlinal_lab2\src\latex\data4.txt', 'r')
+f = open('Z:\practlinal_lab2\src\latex\data8.txt', 'r')
 lines = [line for line in f]
 print("Примеры преобразования точек:")
 print()
@@ -30,24 +32,15 @@ matrB = [[0, 0], [0, 0]]
 matrC = [[0, 0], [0, 0]]
 for i in range(len(lines)):
     m = [[float(num) for num in line.split()] for line in lines[2*i:2*i+2:]]
-    print("(%.3f, %.3f)" % (m[0][0], m[0][1]))
-    print("(%.3f, %.3f)" % (m[1][0], m[1][1]))
     if i < 2:
         matrB[0][i] = m[0][0]
         matrB[1][i] = m[0][1]
         matrC[0][i] = m[1][0]
         matrC[1][i] = m[1][1]
-    #if i == 3:
-        #print("$C \cdot B^{-1} = A$\n")
-        #latex_matr_2x2(matrC)
-        #latex_matr_2x2(matrB)    
-        #print("^{-1}$\n")
-        #print("Для проверки")
+    else:
 
-    #latex_matrix_2x2(m)
-    #latex_vector(m[0])
-    #print("$\\rightarrow$")
-    #latex_vector(m[1])
-    #print()
+        latex_matrix_2x2(m)
+        latex_matr_2x2(m)
+    
 
 
